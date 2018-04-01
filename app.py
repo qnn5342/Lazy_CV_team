1740522392513af3055494e5b7ec8d9230c0eb4b
qnn5342 <quangnhatnguyen2403@gmail.com>
2018-04-02 02:04:51 +0700

    last before New Tab

--- app.py
+++ app.py
@@ -319,32 +319,17 @@ def pdf(Username):
 # def admin():
 #     services = Service.objects()
 #     return render_template('admin.html', services= services)
-
-@app.route('/delete/<service_id>')
-def delete(service_id):
-    service_to_delete = Service.objects.with_id(service_id)
-
-    if service_to_delete is None:
-        return "Not found"
-
-    service_to_delete.delete()
-
-    return redirect(url_for('admin'))
-
-@app.route('/new_service', methods = ['GET','POST'])
-def created():
-    if request.method == 'GET':
-        return render_template('new_service.html')
-    elif request.method == 'POST':
-        form = request.form
-        name = form['name']
-        yob = form['yob']
-        phone = form['phone']
-
-        new_service = Service(name=name, yob = yob, phone = phone, address = "Hà Nội", status = True, gender = 0, height = 169)
-        new_service.save()
-
-        return "Saved"
+#
+# @app.route('/delete/<service_id>')
+# def delete(service_id):
+#     service_to_delete = Service.objects.with_id(service_id)
+#
+#     if service_to_delete is None:
+#         return "Not found"
+#
+#     service_to_delete.delete()
+#
+#     return redirect(url_for('admin'))
 #
 # @app.route('/login')
 # def login():
